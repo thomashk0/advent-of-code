bfcbba8e02881a865dcbea57fd185df8185ea73a
Thomas Hiscock <thomas.hiscock@wanadoo.fr>
2019-12-20 15:24:07 +0100

    2019: solution for day20 (part 2)

--- 2019/python/day20.py
+++ 2019/python/day20.py
@@ -83,6 +83,24 @@ def scan_portals(m):
     return portal_map
 
 
+def portal_info(portals):
+    links = {}
+    for p in portals.values():
+        if len(p) != 2:
+            continue
+        src, dst = tuple(p)
+        assert src not in links
+        other = links.get(dst)
+        assert other is None
+        links[src] = dst
+        links[dst] = src
+    all_portals = {p: k for k, portal in portals.items() for p in portal}
+    (xmin, xmax), (ymin, ymax) = limits(all_portals)
+    outer_portals = {p: k for p, k in all_portals.items() if
+                     p[0] in [xmin, xmax] or p[1] in [ymin, ymax]}
+    return links, outer_portals
+
+
 def build_graph(m, portals, with_shortcuts=True):
     shortcuts = {}
     for p in portals.values():
@@ -116,23 +134,55 @@ def build_graph(m, portals, with_shortcuts=True):
     return g
 
 
-def part_1(f_name):
+def part_1(f_name, debug=False):
     m = parse(f_name)
     portals = scan_portals(m)
-    # print(portals)
-
     g = build_graph(m, portals)
     p = nx.shortest_path(g, portals['AA'][0], portals['ZZ'][0])
-    for coords in p:
-        m[coords] = '*'
-    # draw(m)
+    if debug:
+        for coords in p:
+            m[coords] = '*'
+        draw(m)
     return len(p) - 1
+
+
+def part_2(f_name):
+    m = parse(f_name)
+    portals = scan_portals(m)
+    shortcut, outer = portal_info(portals)
+
+    target = portals['ZZ'][0]
+    to_explore = {(portals['AA'][0], 0)}
+    explored = set()
+    step = 0
+    while True:
+        to_explore_next = set()
+        for node in to_explore:
+            if node[0] == target and node[1] == 0:
+                return step
+            if node in explored:
+                continue
+            explored.add(node)
+            n, level = node
+            for direction, s in neightbours(n):
+                if m.get(s) == '.':
+                    to_explore_next.add((s, level))
+            d = shortcut.get(n)
+            if d:
+                incr = 1 if d in outer else -1
+                next_level = level + incr
+                if next_level >= 0:
+                    to_explore_next.add((d, next_level))
+        to_explore = to_explore_next
+        step += 1
 
 
 def main():
     assert part_1("assets/day20-example-0") == 23
     assert part_1("assets/day20-example-1") == 58
     print("part 1:", part_1("assets/day20-input"))
+    assert part_2("assets/day20-example-2") == 396
+    print("part 2:", part_2("assets/day20-input"))
 
 
 if __name__ == '__main__':
