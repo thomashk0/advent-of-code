6458038d15918689dfba0d5cc97bf50f3902baee
Thomas Hiscock <thomashk000@gmail.com>
2022-12-25 10:40:23 +0100

    2022: day24

--- 2022-py/aoc/day22.py
+++ 2022-py/aoc/day22.py
@@ -1,6 +1,8 @@
 # Skeleton for days
 import re
 
+import aoc
+
 STEP_RE = re.compile(r"(\d+|[LR])")
 
 
@@ -16,6 +18,11 @@ def rotate_l(p):
 
 def rotate_r(p):
     return p[1], -p[0]
+
+
+def adjacent(x, dirs):
+    for dx, dy in dirs:
+        yield x[0] + dx, x[1] + dy
 
 
 def parse_input(raw: str):
@@ -55,30 +62,13 @@ def get_wraps(m):
 FACING_VALUE = {(0, 1): 0, (1, 0): 1, (0, -1): 2, (-1, 0): 3}
 
 
-def part_1(args):
-    m, steps = args
-    wi, wj = get_wraps(m)
-
-    wraps = {}
-    for j, (wmin, wmax) in enumerate(wi):
-        wraps[wmax + 1, j, (1, 0)] = (wmin, j)
-        wraps[wmin - 1, j, (-1, 0)] = (wmax, j)
-    for i, (wmin, wmax) in enumerate(wj):
-        wraps[i, wmax + 1, (0, 1)] = (i, wmin)
-        wraps[i, wmin - 1, (0, -1)] = (i, wmax)
-
-    cursor = (0, wj[0][0])
-
-    cursor, direction = simulate(m, steps, cursor, wraps)
-    return get_password(cursor, direction)
-
-
 def get_password(cursor, direction):
     return 1000 * (1 + cursor[0]) + 4 * (1 + cursor[1]) + FACING_VALUE[direction]
 
 
 def simulate(m, steps, cursor, wraps):
     direction = (0, 1)
+
     for p in steps:
         if p == "L":
             direction = rotate_l(direction)
@@ -97,12 +87,75 @@ def simulate(m, steps, cursor, wraps):
     return cursor, direction
 
 
-def part_2(input):
-    return 2 * len(input)
+def part_1(args):
+    m, steps = args
+    wi, wj = get_wraps(m)
+
+    wraps = {}
+    for j, (wmin, wmax) in enumerate(wi):
+        wraps[wmax + 1, j, (1, 0)] = (wmin, j)
+        wraps[wmin - 1, j, (-1, 0)] = (wmax, j)
+    for i, (wmin, wmax) in enumerate(wj):
+        wraps[i, wmax + 1, (0, 1)] = (i, wmin)
+        wraps[i, wmin - 1, (0, -1)] = (i, wmax)
+
+    cursor = (0, wj[0][0])
+
+    cursor, direction = simulate(m, steps, cursor, wraps)
+    return get_password(cursor, direction)
+
+
+def scan_squares(m, start, width):
+    dirs = [(-width, 0), (width, 0), (0, width), (0, -width)]
+    to_explore = {start}
+    explored = {}
+    s_id = 1
+    while len(to_explore) > 0:
+        head = to_explore.pop()
+        explored[head] = s_id
+        s_id += 1
+        for d in adjacent(head, dirs):
+            if d in explored:
+                continue
+            if d not in m:
+                continue
+            to_explore.add(d)
+    return explored
+
+
+def part_2(args):
+    m, steps = args
+    wi, wj = get_wraps(m)
+
+    square_width = 4
+    cursor = (0, wj[0][0])
+    squares = scan_squares(m, cursor, square_width)
+    debug = {}
+    for loc, s_id in squares.items():
+        for i in range(square_width):
+            for j in range(square_width):
+                debug[loc[1] + j, loc[0] + i] = f"{s_id}"
+    print("squares:", squares)
+    d = aoc.SparseMap(debug, default=" ")
+    
+    print(d.draw())
+    #
+    # regions = {}
+    # for i in range(len(wj)):
+    #     for j in range(len(wi)):
+    #         if (i, j) in m:
+    #             wrap_i = wi[j]
+    #             wrap_j = wj[i]
+    #             regions[i, j] = (wrap_i, wrap_j)
+
+    # unique_regions =
+    # for i in range(len(wj)):
+    #     for j in range(len(wi)):
+    return 2 * len(args)
 
 
 def aoc_inputs():
     return {
-        "example": ("day22-input-ex", 6032, 8),
-        "real": ("day22-input-1", 164014, None),
+        "example": ("day22-input-0", 6032, 8),
+        # "real": ("day22-input-1", 164014, None),
     }
---
+++ 2022-py/aoc/day24.py
@@ -0,0 +1,144 @@
+# Skeleton for days
+import collections
+import copy
+import itertools
+from dataclasses import dataclass
+from typing import Any
+
+import aoc
+
+DIRS = {
+    "<": (0, -1),
+    ">": (0, 1),
+    "^": (-1, 0),
+    "v": (1, 0)
+}
+
+
+def adjacent(x, dirs):
+    for dx, dy in dirs:
+        yield x[0] + dx, x[1] + dy
+
+
+def tuple_add(p, q):
+    return tuple(x + y for x, y in zip(p, q))
+
+
+def tuple_mul(p, q):
+    return tuple(x * y for x, y in zip(p, q))
+
+
+def tuple_reverse(x):
+    return tuple(reversed(x))
+
+
+@dataclass
+class Context:
+    data: Any
+    busy_cells: Any
+    blizzards: Any
+    shape: Any
+
+
+def parse_input(raw: str):
+    lines = raw.splitlines()
+    blizzards = []
+    data = {}
+    for i, line in enumerate(lines):
+        for j, c in enumerate(line):
+            if c in ["<", ">", "v", "^"]:
+                blizzards.append(((i, j), c))
+                data[i, j] = "."
+            else:
+                data[i, j] = c
+    return Context(data=data, busy_cells=set((i, j) for (i, j), _ in blizzards), blizzards=blizzards,
+                   shape=(len(lines), len(lines[0])))
+
+
+def update_blizzards(ctx: Context):
+    busy = collections.defaultdict(int)
+    new_blizzards = []
+    for (i, j), d in ctx.blizzards:
+        move_dir = DIRS[d]
+        ni, nj = tuple_add((i, j), move_dir)
+        # Wrapping rules...
+
+        v = ctx.data.get((ni, nj), None)
+        if v is None or v == "#":
+            sh = tuple_mul(move_dir, (-(ctx.shape[0] + 1), -(ctx.shape[1] + 1)))
+            start = tuple_add((ni, nj), sh)
+            while ctx.data.get(start, None) != ".":
+                start = tuple_add(start, move_dir)
+            ni, nj = start
+        busy[(ni, nj)] += 1
+        new_blizzards.append(((ni, nj), d))
+    ctx.blizzards = new_blizzards
+    ctx.busy_cells = busy
+
+
+def part_1(ctx: Context):
+    ctx = copy.deepcopy(ctx)
+    start = (0, 1)
+    end = (ctx.shape[0] - 1, ctx.shape[1] - 2)
+    assert ctx.data[start] == "."
+    assert ctx.data[end] == "."
+    print(ctx.shape)
+    print(ctx.blizzards)
+    print(ctx.data)
+
+    found = move_to(ctx, end, start)
+    return found
+
+
+def move_to(ctx, target, src):
+    locs = {src}
+    found = None
+    # my_loc = start
+    for i in range(10000):
+        update_blizzards(ctx)
+        if target in locs:
+            found = i
+            break
+        new_locs = set()
+        for head in locs:
+            for head_new in itertools.chain(adjacent(head, DIRS.values()), [head]):
+                if head_new in ctx.busy_cells:
+                    continue
+                if ctx.data.get(head_new, None) != ".":
+                    continue
+                new_locs.add(head_new)
+        locs = new_locs
+        # debug = aoc.SparseMap({tuple_reverse(k): v for k, v in ctx.data.items()}, default=".")
+        # for loc, d in ctx.blizzards:
+        #     if ctx.busy_cells[loc] > 1:
+        #         debug[tuple_reverse(loc)] = f"{ctx.busy_cells[loc]}"
+        #     else:
+        #         debug[tuple_reverse(loc)] = d
+        #     for loc in new_locs:
+        #         debug[tuple_reverse(loc)] = "X"
+        # print(f"After step: {i}")
+        # print(debug.draw())
+        # print()
+    return found
+
+
+def part_2(ctx):
+    start = (0, 1)
+    end = (ctx.shape[0] - 1, ctx.shape[1] - 2)
+    assert ctx.data[start] == "."
+    assert ctx.data[end] == "."
+    print(ctx.shape)
+    print(ctx.blizzards)
+    print(ctx.data)
+
+    found0 = move_to(ctx, end, start)
+    found1 = move_to(ctx, start, end)
+    found2 = move_to(ctx, end, start)
+    return found0 + found1 + found2 + 2
+
+
+def aoc_inputs():
+    return {
+        "example": ("day24-input-ex", 18, 54),
+        "real": ("day24-input-1", 230, None)
+    }
