66e3ebaefaee34fe9c2136aced983614b6089bc6
Thomas Hiscock <thomashk000@gmail.com>
2022-12-27 10:56:51 +0100

    2022: day22 part 2

--- 2022-py/aoc/day22.py
+++ 2022-py/aoc/day22.py
@@ -84,9 +84,11 @@ def simulate(m, steps, cursor, apply_wrapping):
             direction = rotate_r(direction)
         else:
             for i in range(p):
-                n_i, n_j = apply_wrapping(cursor, direction)
+                (n_i, n_j), new_dir = apply_wrapping(cursor, direction)
                 if m[n_i, n_j] == "#":
                     break
+                if new_dir is not None:
+                    direction = new_dir
                 cursor = n_i, n_j
     return cursor, direction
 
@@ -110,95 +112,115 @@ def part_1(args):
         n_j = src[1] + dir[1]
         wrapped = wraps.get((n_i, n_j, dir))
         if wrapped is not None:
-            return wrapped
-        return n_i, n_j
+            return wrapped, None
+        return (n_i, n_j), None
 
     cursor, direction = simulate(m, steps, cursor, handle_wrap)
     return get_password(cursor, direction)
 
 
 def scan_squares(m, start, width):
-    cube_map = {start: "T"}
     dirs = [(-width, 0), (width, 0), (0, width), (0, -width)]
     to_explore = {start}
     explored = {}
     s_id = 1
     while len(to_explore) > 0:
         head = to_explore.pop()
-        head_face = cube_map[head]
         explored[head] = s_id
         s_id += 1
-        for d, (di, dj) in zip(adjacent(head, dirs), dirs):
+        for d in adjacent(head, dirs):
             if d in explored:
                 continue
             if d not in m:
                 continue
-            cube_map[d] = CUBE_MAP[head_face][di // width, dj // width]
             to_explore.add(d)
-    return explored, cube_map
-
-
-#  T
-#  S
-#  B
-#  N
-#  T
-#  ENWSE
-CUBE_MAP = {
-    "T": {(-1, 0): "N", (1, 0): "S", (0, 1): "E", (0, -1): "W"},
-    "N": {(-1, 0): "B", (1, 0): "T", (0, 1): "E", (0, -1): "W"},
-    "S": {(-1, 0): "T", (1, 0): "B", (0, 1): "E", (0, -1): "W"},
-    "B": {(-1, 0): "S", (1, 0): "N", (0, 1): "E", (0, -1): "W"},
-    "E": {(-1, 0): "T", (1, 0): "B", (0, 1): "N", (0, -1): "S"},
-    "W": {(-1, 0): "T", (1, 0): "B", (0, 1): "S", (0, -1): "N"}
-}
-
-
-def cube_wrap(cube_origins, cube_ids, src, d, square_width):
-    q = tuple_add(src, d)
-    if q in cube_ids:
-        return q
-    # PHASE0: wrap inside the cube, then translate.
-    cube_offset = tuple_sub(q, cube_origins[cube_ids[src]])
-    wrapped = cube_offset[0] % square_width, cube_offset[1] % square_width
-    new_cube = CUBE_MAP[cube_ids[src]][d]
-    new_loc = tuple_add(cube_origins[new_cube], wrapped)
-    return new_loc
+    return explored
 
 
 def part_2(args):
     m, steps = args
     wi, wj = get_wraps(m)
 
-    square_width = 4
+    square_width = 50
     cursor = (0, wj[0][0])
-    squares, cube_map = scan_squares(m, cursor, square_width)
-    print("cube_map:", cube_map)
+    squares = scan_squares(m, cursor, square_width)
+    # print("cube_map:", cube_map)
     debug = {}
     for loc, s_id in squares.items():
-        ch = cube_map[loc]
+        # ch = cube_map[loc]
         for i in range(square_width):
             for j in range(square_width):
-                debug[loc[0] + i, loc[1] + j] = ch
-
+                debug[loc[0] + i, loc[1] + j] = s_id
     print("squares:", squares)
-    cube_origins = {v: k for k, v in cube_map.items()}
-    print("origins:", cube_origins)
-    d = aoc.SparseMap({(j, i): v for (i, j), v in debug.items()}, default=" ")
-    print(d.draw())
+    squares_origins = {v: k for k, v in squares.items()}
+    print("square origins:", squares_origins)
+
+    # cube_origins = {v: k for k, v in cube_map.items()}
+    # print("origins:", cube_origins)
+    # d = aoc.SparseMap({(j, i): str(v) for (i, j), v in debug.items()}, default=" ")
+    # print(d.draw())
 
     def handle_wrap(src, dir):
-        return cube_wrap(cube_origins, debug, src, dir, square_width)
+        p = tuple_add(src, dir)
+        if p in debug:
+            return p, None
+        delta = tuple_sub(src, squares_origins[debug[src]])
+        if debug[src] == 1 and dir == (-1, 0):
+            cube = squares_origins[6]
+            return (cube[0] + delta[1], cube[1]), (0, 1)
+        if debug[src] == 1 and dir == (0, -1):
+            cube = squares_origins[5]
+            return (cube[0] + square_width - 1 - delta[0], cube[1]), (0, 1)
+        if debug[src] == 6 and dir == (0, -1):
+            cube = squares_origins[1]
+            return (cube[0], cube[1] + delta[0]), (1, 0)
+        if debug[src] == 5 and dir == (0, -1):
+            cube = squares_origins[1]
+            return (cube[0] + square_width - 1 - delta[0], cube[1]), (0, 1)
+        if debug[src] == 5 and dir == (-1, 0):
+            cube = squares_origins[2]
+            return (cube[0] + delta[1], cube[1]), (0, 1)
+        if debug[src] == 2 and dir == (0, -1):
+            cube = squares_origins[5]
+            return (cube[0], cube[1] + delta[0]), (1, 0)
+        if debug[src] == 2 and dir == (0, 1):
+            cube = squares_origins[4]
+            return (cube[0] + square_width - 1, cube[1] + delta[0]), (-1, 0)
+        if debug[src] == 4 and dir == (0, 1):
+            cube = squares_origins[3]
+            return (
+                cube[0] + square_width - 1 - delta[0],
+                cube[1] + square_width - 1,
+            ), (0, -1)
+        if debug[src] == 6 and dir == (1, 0):
+            cube = squares_origins[4]
+            return (cube[0], cube[1] + delta[1]), (1, 0)
+        if debug[src] == 6 and dir == (0, 1):
+            cube = squares_origins[3]
+            return (cube[0] + square_width - 1, cube[1] + delta[0]), (-1, 0)
+        if debug[src] == 4 and dir == (1, 0):
+            cube = squares_origins[2]
+            return (cube[0] + delta[1], cube[1] + square_width - 1), (0, -1)
+        if debug[src] == 3 and dir == (1, 0):
+            cube = squares_origins[6]
+            return (cube[0] + delta[1], cube[1] + square_width - 1), (0, -1)
+        if debug[src] == 3 and dir == (0, 1):
+            cube = squares_origins[4]
+            return (
+                cube[0] + square_width - 1 - delta[0],
+                cube[1] + square_width - 1,
+            ), (0, -1)
+        if debug[src] == 4 and dir == (-1, 0):
+            cube = squares_origins[6]
+            return (cube[0] + square_width - 1, cube[1] + delta[1]), (-1, 0)
+        raise NotImplemented()
 
     cursor = (0, wj[0][0])
     cursor, direction = simulate(m, steps, cursor, handle_wrap)
     return get_password(cursor, direction)
 
-    return 2 * len(args)
-
 
 def aoc_inputs():
     return {
-        "example": ("day22-input-ex", 6032, 8),
-        # "real": ("day22-input-1", 164014, None),
+        "real": ("day22-input-1", 164014, 47525),
     }
